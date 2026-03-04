4927143c81273fd6f6abef11689f475ae0444a9d
John Adebayo <johnayomide31@gmail.com>
2026-03-04 07:42:59 +0100

    making production work

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,17 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Clearing all caches..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Optimizing autoloader..."
+composer dump-autoload --optimize --no-dev --classmap-authoritative
